a108644f56b7410946d73f1014b67ad26d5df75b
alextudosa <tudosa_alex@yahoo.com>
2019-06-25 09:51:18 +0300

    implement buttons and create game strategy

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,15 +2,78 @@ package com.example.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button buttons[][] = new Button[3][3];
 
+    private boolean player1Turn = true;
+
+    private  int roundCount;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView player1TextView;
+    private TextView player2TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player1TextView = findViewById(R.id.txtview_player1);
+        player2TextView = findViewById(R.id.txtview_player2);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+
+        Button buttonRestart = findViewById(R.id.restartgame_button);
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
     }
+
+        @Override
+        public void onClick(View v) {
+            if(!((Button) v).getText().toString().equals("")) {
+                return;
+            }
+
+            if(player1Turn){
+                ((Button) v).setText("X");
+            }else{
+                ((Button) v).setText("O");
+            }
+
+            roundCount++;
+
+            if(checkForWin()) {
+                if (player1Turn) {
+                    player1Wins();
+                } else {
+                    player2Wins();
+                }
+            }else if(roundCount == 9){
+                    draw();
+                }else{
+                    player1Turn = !player1Turn;
+                }
+            }
+
+
+
 }
